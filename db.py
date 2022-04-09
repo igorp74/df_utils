ee42dc2d0186e38c8bb8990ca0a52b07c9f44c50
Igor Perkovic <igorp.mailbox@gmail.com>
2022-04-10 00:25:17 +0200

    DB parameters unification

--- db.py
+++ db.py
@@ -14,14 +14,14 @@
 """
 
 
-def mssqlsrv(sqlserver, database, **kwargs):
+def mssqlsrv(hostname, database, **kwargs):
     """
     #*MicroSoft SQL Server connection engine for Python.*
 
     ---
     ### Arguments:
     **Mandatory**
-    - sqlserver:    Host IP or Name. Non-default port number goes after coma.
+    - hostname:     Host IP or Name. Non-default port number goes after coma.
     - database:     Database name
 
     **Optional** (defaults are in bold)
@@ -48,10 +48,6 @@ def mssqlsrv(sqlserver, database, **kwargs):
         #print("%s = %s" % (k, v))
 
         #------------------------
-        # if k == 'a_server':
-        #     sqlserver = v
-        # if k == 'a_database':
-        #     database  = v
         if k == 'a_mode':
             mode = v
         if k == 'a_autocommit':
@@ -61,24 +57,27 @@ def mssqlsrv(sqlserver, database, **kwargs):
         if k == 'a_password':
             password = v
 
-    #---------------------------------------------------------------------------
-    conn_string_base = "DRIVER={SQL Server};SERVER="+sqlserver+";DATABASE="+database
-    #---------------------------------------------------------------------------
+    #-------------------------------------------------------------------------------------
+    conn_string_base = "DRIVER={SQL Server};SERVER=" + hostname + ";DATABASE=" + database
+    #-------------------------------------------------------------------------------------
     if  (len(username)>0) & (len(password)>0):
         conn_string = conn_string_base+";UID="+username+";PWD="+password
     else:
         conn_string = conn_string_base+";Trusted_Connection=Yes;"
 
     try:
+        print('📢 CONNECTING TO:')
+        print('   Hostname   :', hostname)
+        print('   Database   :', database)
+
         if mode == 'conn':
             import pyodbc
 
             # SQL Server Database Connection
             result = pyodbc.connect(conn_string, autocommit=ac)
 
-            print('📢 DB Connection SUCCESS on:', sqlserver, database)
-            print('----------------------------------------------------------------')
-            print('CONNECTION mode, AUTOCOMMIT:', ac, '\n')
+            print('   Mode       : CONNECTION')
+            print('   Autocommit : {ac}')
 
             c = result.cursor()
 
@@ -91,21 +90,20 @@ def mssqlsrv(sqlserver, database, **kwargs):
             params = urllib.parse.quote(conn_string)
             result  = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
 
-            print('📢 DB Connection SUCCESS on:', sqlserver, database)
-            print('----------------------------------------------------------------')
-            print('ENGINE mode\n')
+            print('   Mode       : ENGINE')
 
             conn = result.raw_connection()
             c = conn.cursor()
 
         c.execute('SELECT @@version')
         res = c.fetchall()
-        print(res[0][0])
+        print('✅ ',res[0][0])
 
         return result
 
-    except:
-        print('DB Connection ERROR\n')
+    except Exception as ex:
+        print('----------------------------------------------------------------')
+        print('❌ DB Connection ERROR ❌\n  ',ex)
 
 def oracle(hostname, database, **kwargs):
     """
@@ -150,6 +148,9 @@ def oracle(hostname, database, **kwargs):
             password = v
 
     try:
+        print('📢 Connecting to:', hostname, database)
+        print('----------------------------------------------------------------')
+
         if mode == 'conn':
             import cx_Oracle
 
@@ -158,16 +159,14 @@ def oracle(hostname, database, **kwargs):
             conn = cx_Oracle.connect(username, password, hostname+'/'+database, encoding="UTF-8")
             # ------------------------------------------------------------------------------------
 
-            print('Connecting to database...')
             cur = conn.cursor()
             cur.execute ('SELECT * FROM v$version')
             result = cur.fetchone()
             cur.close()
 
-            print('----------------------------------------------------------------------------')
             print('📢 DB Connection SUCCESS on:', database)
-            print(result[0])
             print('----------------------------------------------------------------------------\n')
+            print(result[0])
 
             return conn
 
@@ -197,6 +196,7 @@ def postgresql(hostname, database, **kwargs):
     mode      = 'engine'
     username  = ''
     password  = ''
+    c         = ''
 
     # Get dynamic argument
     for k,v in kwargs.items():
@@ -213,38 +213,39 @@ def postgresql(hostname, database, **kwargs):
             password = v
 
     try:
+        print('📢 CONNECTING TO:')
+        print('   Hostname :', hostname)
+        print('   Database :', database)
+
         if mode == 'conn':
             #  Define database connection
-            conn = pg.connect(f'host={hostname} dbname={database} user={username} password={password}')
+            result = pg.connect(f'host={hostname} dbname={database} user={username} password={password}')
+
             # Checking database connection
             #-----------------------------------------------------
-            cursor = conn.cursor()
-            #Executing an SQL function using the execute() method
-            cursor.execute("select version()")
-            # Fetch a single row using fetchone() method.
-            data = cursor.fetchone()
-
-            print('📢 DB Connection SUCCESS on:', data[0])
-            print('----------------------------------------------------------------')
-            print('CONN mode\n')
-
-            return conn
+            c = result.cursor()
+            print('   Mode     : CONNECTION')
 
         if mode == 'engine':
             from sqlalchemy import create_engine
-            import pandas as pd
 
             result = create_engine(f"postgresql+psycopg2://{username}:{password}@{hostname}/{database}")
+            print('   Mode     : ENGINE')
 
-            print('📢 DB Connection SUCCESS on:', hostname, database)
-            print('----------------------------------------------------------------')
-            print('ENGINE mode\n')
+            conn = result.raw_connection()
+            c = conn.cursor()
 
-            df = pd.read_sql('SELECT version()', con=result)
-            print(df.values[0][0])
-            print('\n')
+        c.execute("select version()")
+        data = c.fetchone()
 
-            return result
+        #from tabulate import tabulate
+        #table = [['Hostname',hostname],['Database',database],['Mode','ENGINE']]
+        #print(tabulate(table, tablefmt='psql'))
 
-    except:
-        print('DB Connection ERROR\n')
+        print(f'✅ {data[0]}\n')
+
+        return result
+
+    except Exception as ex:
+        print('----------------------------------------------------------------')
+        print('❌ DB Connection ERROR ❌\n  ',ex)
