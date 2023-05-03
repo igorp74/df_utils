1606f2cf074be3252f6ff861a4f04af2d3f16fa6
Igor Perkovic <igorp.mailbox@gmail.com>
2023-05-03 23:05:29 +0100

    get_xlsx improved
    get_xlsx improved
    df_2_xlsx improved (automatic recognition of multiple or single dataframes)

--- du.py
+++ du.py
@@ -6,18 +6,17 @@
 👔 by Igor Perkovic
 
 🛠 CREATED: 2020-10-13 08:39:29
-📆 CHANGED: 2022-04-13 17:50:56
+📆 CHANGED: 2023-05-03 09:21:15
 
 ---
 ⚙ PREREQUISITES:
 📘 Libraries: xlsxwriter, pandas, sqlalchemy
 
 """
-
+from datetime import datetime, timedelta
+from pathlib import Path
 import numpy as np
 import pandas as pd
-
-from datetime import datetime, timedelta
 
 
 # Databases
@@ -79,7 +78,7 @@ def df_2_sqlite(df, db_path, table_name):
         df.to_sql(table_name, sqlite_connection, if_exists='fail')
         print(f'Successfully created database {db_path} and table {table_name}')
     except:
-        print('❌❌❌ Error creating SQLite database !❌❌❌')
+        print('ERROR creating SQLite database !')
 
 
 
@@ -135,25 +134,39 @@ def get_xlsx(fn, **kwargs):
     ――――――――――――――――――――――――――――――――――――――――――――――――――
     fn (Path)            Existing xlsx file
 
-    a_sn_list (list)     List of desired sheet names
-    a_sn_col  (str)      Column name for column with worksheet names
-    a_header_rows (int)  Number of rows for handling multiple headers
-    a_delimiter   (str)  Delimiter for merged header names
-    a_destination (Path) Path and name of pickle file for saving the data localy
+    sn_list     (list) List of desired existing sheet names
+    sn_col      (str)  Column name for column with worksheet names
+    header_rows (int)  Number of rows for handling multiple headers
+    delimiter   (str)  Delimiter for merged header names
+    clean       (Bool) Remove empty columns and rows from dataframes
+    collect     (Bool) Collect dataframes of all sheets into list of dataframes
+    concatenate (Bool) Concatenate all work sheets
+    to_pickle   (Bool) Save each worksheet in a pickle file
+    destination (Path) Path for saving pickle files
 
     🎯 RETURNS
     ――――――――――――――――――――――――――――――――――――――――――――――――――
     → List of DataFrames - one DataFrame from each worksheet
     """
 
+    xlsx = pd.ExcelFile(fn)
+    sheets = xlsx.sheet_names
+    print(sheets)
+
     # Default values
     #-----------------
-    sn = []     # sheet names
+    sn = []     # sheet names - which I want to read
+    work_sheets = []
     snc = ''
     nr = 0
     delimiter = '-'
     src_headers = 0
-    dest_pick = ''
+    collect = 0
+    p = 0 # pickle
+    c = 0 # clean
+    con = 0 # concatenate
+    dest_pick = Path('')
+
 
     # Get dynamic argument
     for k,v in kwargs.items():
@@ -161,51 +174,80 @@ def get_xlsx(fn, **kwargs):
         # ---------------------------
         # print("%s = %s" % (k, v))
 
-        if k == 'a_sn_list':
+        if k == 'collect':
+            collect = v
+        if k == 'to_pickle':
+            p = v
+        if k == 'sn_list':
             sn  = v
-        if k == 'a_sn_col':
+        if k == 'sn_col':
             snc  = v
-        if k == 'a_header_rows':
+        if k == 'header_rows':
             nr = v
-        if k == 'a_delimiter':
+        if k == 'delimiter':
             delimiter = v
-        if k == 'a_destination':
+        if k == 'clean':
+            c = v
+        if k == 'concatenate':
+            con = v
+        if k == 'destination':
             dest_pick = v
 
     if nr > 1:
         src_headers = [i for i in range(nr)]
 
-    xlsx = pd.ExcelFile(fn)
-    sheets = xlsx.sheet_names
-    print(sheets)
-
     dfs = []
-    if sn:
-        for ws in sn:
-            print('Reading worksheet:',ws)
-            print('headers: ', src_headers)
-            df = xlsx.parse(ws, header=src_headers)
-            if len(snc):
-                df[snc] = ws
-            if nr > 1:
-                cl = df.columns.tolist()
-                df.columns = df_merged_headers(cl, delimiter)
+
+    if len(sn):
+        work_sheets = sn
+    else:
+        work_sheets = sheets
+
+    for ws in work_sheets:
+        print('Reading worksheet:',ws)
+        print('Headers: ', src_headers)
+
+        df = xlsx.parse(ws, header=src_headers)
+
+        if len(snc):
+            df[snc] = ws
+
+        if nr > 1:
+            cl = df.columns.tolist()
+            df.columns = df_merged_headers(cl, delimiter)
+
+        # Clean
+        if c:
+            df = df.dropna(axis=1, how='all')
+            df = df.dropna(axis=0, how='all')
+            df = df.reset_index(drop=True)
+
+        # Collect DataFrames into dataframe list
+        if collect:
             print('Appending in general list...')
             dfs.append(df)
 
-        res = pd.concat(dfs)
-        res.columns = res.columns.str.replace('\n', '')
+        # Save worksheets to pickle files
+        if p > 0 and con == 0:
+            tmp_fn = fn.stem + '_' + ws + '.pickle'
+            tmp_pn = dest_pick / tmp_fn
+            df.to_pickle(tmp_pn)
+            print('Created:', tmp_pn)
 
-        res.reset_index(drop=True, inplace=True)
-        res.dropna(axis=1, how='all', inplace=True)
-        res.dropna(axis=0, how='all', inplace=True)
-
-
-        if len(dest_pick)>1:
-            res.to_pickle(dest_pick)
-            print('Created:',dest_pick)
+    if collect:
+        if con:
+            res = pd.concat(dfs)
+            res.columns = res.columns.str.replace('\n', '')
+            if p:
+                tmp_fn = fn.stem + '_unified.pickle'
+                tmp_pn = dest_pick / tmp_fn
+                df.to_pickle(tmp_pn)
+            else:
+                return res
         else:
-            return res
+            return dfs
+    else:
+        return df
 
 
 def from_excel_ordinal(ordinal, _epoch0=datetime(1899, 12, 31)):
@@ -261,9 +303,9 @@ def df_append_2_xlsx(df, file_name, sheet_name):
             new_sheet.cell(row=1, column=y).fill = bg_style
 
         writer.save()
-        print(f'✔ Successfully append sheet: {sheet_name}\n   to file: {file_name}')
+        print(f'Successfully append sheet: {sheet_name}\n   to file: {file_name}')
     except:
-        print(f'❌❌❌ Cannot open or write to file: {file_name}.\nIs it open ?')
+        print(f'Cannot open or write to file: {file_name}.\nIs it open ?')
 
 
 def df_2_xlsx(df, fn, sn, **kwargs):
@@ -300,7 +342,6 @@ def df_2_xlsx(df, fn, sn, **kwargs):
     if 'list' in str(type(df)):
         tcl = [tc]*len(df)
     ac  = 1
-    m   = 0
     s   = 0
     ind = False
     ts  = 'Table Style Medium 2'
@@ -322,8 +363,6 @@ def df_2_xlsx(df, fn, sn, **kwargs):
             s = v
         if k == 'a_table_style':
             ts = v
-        if k == 'a_multi':
-            m = v
         if k == 'a_properties':
             wsp = v
         if k == 'a_index':
@@ -354,7 +393,51 @@ def df_2_xlsx(df, fn, sn, **kwargs):
         }
 
 
-    if m==0:
+    # Check if list of DataFrames is passed in argument
+    if 'list' in str(type(df)):
+        if len(df) == len(sn):
+            for d,s, tagc in zip(df, sn, tcl):
+
+                # Skip empty worksheets
+                if d.empty:
+                    pass
+                else:
+                    d.to_excel(writer, sheet_name=s, index=False)
+
+                    ws = writer.sheets[s]
+                    for i, width in enumerate(get_col_widths(d)):
+                        ws.set_column(i-1, i-1, width+2)
+
+                    if s:
+                        # Table style format
+                        #--------------------
+                        cols = []
+                        for c in (d.columns.values):
+                            cols.append({'header': c})
+
+                        ws.add_table(0, 0, len(d.index), len(d.columns)-1, {'columns': cols, 'style': ts})
+
+                    else:
+                        # Header formatting (option instead of Table Style)
+                        # -------------------------------------------------
+                        header_format = workbook.add_format(format_dict)
+
+                        for col_num, value in enumerate(d.columns.values):
+                            ws.write(0, col_num, value, header_format)
+
+                # Color the tabs
+                ws.set_tab_color(tagc)
+                # Freeze 1st row
+                ws.freeze_panes(1, 0)
+                # Worksheet zoom level
+                ws.set_zoom(80)
+
+        else:
+            print('List of DataFrames | WorkSheets are not of the same size.')
+            print('Exiting...')
+            exit()
+    # If not, check if the single DataFrame is actually a DataFrame type
+    else:
         if 'DataFrame' in str(type(df)):
 
             # Create worksheet Data with data from dataframe
@@ -398,56 +481,10 @@ def df_2_xlsx(df, fn, sn, **kwargs):
             print('This is not a single dataframe for process.')
             print('Exiting...')
             exit()
-    else:
-        if 'list' in str(type(df)):
-            if len(df) == len(sn):
-                for d,s, tagc in zip(df, sn, tcl):
 
-                    # Skip empty worksheets
-                    if d.empty:
-                        pass
-                    else:
-                        d.to_excel(writer, sheet_name=s, index=False)
-
-                        ws = writer.sheets[s]
-                        for i, width in enumerate(get_col_widths(d)):
-                            ws.set_column(i-1, i-1, width+2)
-
-                        if s:
-                            # Table style format
-                            #--------------------
-                            cols = []
-                            for c in (d.columns.values):
-                                cols.append({'header': c})
-
-                            ws.add_table(0, 0, len(d.index), len(d.columns)-1, {'columns': cols, 'style': ts})
-
-                        else:
-                            # Header formatting (option instead of Table Style)
-                            # -------------------------------------------------
-                            header_format = workbook.add_format(format_dict)
-
-                            for col_num, value in enumerate(d.columns.values):
-                                ws.write(0, col_num, value, header_format)
-
-                    # Color the tabs
-                    ws.set_tab_color(tagc)
-                    # Freeze 1st row
-                    ws.freeze_panes(1, 0)
-                    # Worksheet zoom level
-                    ws.set_zoom(80)
-
-            else:
-                print('List of DataFrames | WorkSheets are not of the same size.')
-                print('Exiting...')
-                exit()
-        else:
-            print('This is not a list of dataframes!')
-            print('Exiting...')
-            exit()
     try:
-        writer.save()
-        print('\n ✔Successfully saved: 💾',fn)
+        writer._save()
+        print('\nSuccessfully saved: ',fn)
     except xlsxwriter.exceptions.FileCreateError:
         print('\n\nERROR!!!\nCannot write in opened file.\nCLOSE THE FILE, PLEASE!\n')
 
@@ -470,7 +507,6 @@ def print_df(df, **kwargs):
     vt Values tabular    int
             1 = simple
             2 = psql
-            3 = fancy_grid
     e  Exit after print  int
 
     🎯 RETURNS:
@@ -510,10 +546,10 @@ def print_df(df, **kwargs):
             a_v = 0
 
 
-    print('\n📌 DATAFRAME INFO | Rows:', df.shape[0], 'Columns:', df.shape[1])
+    print('\nDATAFRAME INFO | Rows:', df.shape[0], 'Columns:', df.shape[1])
 
     if a_c:
-        print('🟢 Columns:',df.columns.tolist(),'\n')
+        print('Columns:',df.columns.tolist(),'\n')
 
     if a_d:
         print (df.dtypes,'\n')
@@ -538,8 +574,6 @@ def print_df(df, **kwargs):
             print(tabulate(df,headers=df.columns.tolist(),  tablefmt='simple',showindex=False),'\n')
         elif a_vt == 2:
             print(tabulate(df,headers=df.columns.tolist(),  tablefmt='psql',showindex=False),'\n')
-        elif a_vt == 3:
-            print(tabulate(df,headers=df.columns.tolist(), tablefmt='fancy_grid',showindex=False),'\n')
 
     if a_v:
         print('\n')
